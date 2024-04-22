0f64e71a5dc4f0982ae48e2f05c25cc6b7779f47
Christopher D'Angelo <chris@mathhead200.com>
2024-04-22 16:26:12 -0400

    Simon's oracle completed. Still need to add post-procssing

--- simon/simon.py
+++ simon/simon.py
@@ -10,12 +10,37 @@ n = 3
 
 def f(x):
     """ Our 2-1 function with iff f(x) = f(y) then x + y = 0,s """
-    return x % 4
+    return x % 2**(n-1)
+
+# ----- DON'T EDIT BELOW! -----
+N = 2**n
+I = [[i == j for j in range(N)] for i in range(N)]  # N-bit identity matrix.
+
+def tensor(v: list, w: list, op = lambda v0, w0: v0 * w0):
+    return [v[i] * w[j] for j in range(len(w)) for i in range(len(v))]
+
+def _Bf_column(x: int, y: int) -> list[int]:
+    """ y and x must be n*-bit classical states; i.e., unsigned ints (*see global, n) """
+    return tensor(I[x], I[f(x) ^ y])
+    
+def transpose(M: list[list]) -> list[list]:
+    """ M must be a rectangular matrix; i.e., non-raggid """
+    ROWS = len(M)
+    COLS = len(M[0])
+    return [[M[j][i] for j in range(ROWS)] for i in range(COLS)]
+
+# build Simon's black box
+Bf = transpose([_Bf_column(x, y) for y in range(N) for x in range(N)])
+
+def print_mat(M: list[list]) -> None:
+    for i in range(len(M)):
+        for j in range(len(M[i])):
+            print(M[i][j], end="")
+        print()
 
 if __name__ == "__main__":
-    # TODO: build Simon's black box
-    Uf = [[1 if i == j else 0 for j in range(2**(2*n))] for i in range(2**(2*n))]
-    Uf = UnitaryGate(Uf, label="Uf")
+    print_mat(Bf)
+    Uf = UnitaryGate(Bf, label="Bf")
 
     x = QuantumRegister(n, "x")
     y = QuantumRegister(n, "y")
